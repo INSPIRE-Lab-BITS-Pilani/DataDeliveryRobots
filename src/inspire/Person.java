70f1d19a0945841824113075c2e888e9ae7cd439
Abhinav Baid <abhinavbaid@gmail.com>
2016-10-11 15:49:17 +0530

    Add docs for the Person class

--- src/inspire/Person.java
+++ src/inspire/Person.java
@@ -2,19 +2,33 @@ package inspire;
 
 import java.util.Objects;
 
+/**
+ * This class stores information about a single client - with fields for a human-readable identifier {@code name} and
+ * the host name of the client {@code hostName}.
+ */
 class Person {
     private String name;
     private String hostName;
 
+    /**
+     * @param name Human-readable identifier for the client
+     * @param hostName The client's host name
+     */
     Person(String name, String hostName) {
         this.name = name;
         this.hostName = hostName;
     }
 
+    /**
+     * @return Human-readable identifier for the client
+     */
     String getName() {
         return name;
     }
 
+    /**
+     * @return The client's host name
+     */
     String getHostName() {
         return hostName;
     }
