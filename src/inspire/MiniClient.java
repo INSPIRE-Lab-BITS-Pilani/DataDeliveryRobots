10493056fb6cd856e379ebc59040bbb274c73966
Abhinav Baid <abhinavbaid@gmail.com>
2016-10-16 15:36:12 +0530

    Add comments in the run method of MiniClient

--- src/inspire/MiniClient.java
+++ src/inspire/MiniClient.java
@@ -39,39 +39,51 @@ class MiniClient implements Runnable {
     @Override
     public void run() {
         try {
+            // The number of receiver clients (applicable to Server only)
             int receiverSize = dis.readInt();
             StringBuilder sb;
+            // Map from client host names to the file names that need to be sent to them (applicable to Server only)
             Map<String, Queue<String>> receiverFileList = new HashMap<>();
             if (clientFileListMap != null && receiverSize != 0) {
                 for (int i = 0; i < receiverSize; i++) {
+                    // Length of the receiver client host name
                     int receiverNameLen = dis.readInt();
                     sb = new StringBuilder();
                     for (int j = 0; j < receiverNameLen; j++) {
                         sb.append(dis.readChar());
                     }
+                    // The actual client name
                     String receiverName = sb.toString();
                     receiverFileList.put(receiverName, clientFileListMap.containsKey(receiverName)
                             ? clientFileListMap.get(receiverName) : new ConcurrentLinkedQueue<String>());
                 }
             }
+            // Number of files to receive
             int numberOfFiles = dis.readInt();
             for (int i = 0; i < numberOfFiles; i++) {
+                // Length of the file name
                 int filenameLen = dis.readInt();
                 sb = new StringBuilder();
                 for (int j = 0; j < filenameLen; j++) {
                     sb.append(dis.readChar());
                 }
+                // The actual file name
                 String actualFileName = sb.toString();
                 if (clientFileListMap != null && receiverSize != 0) {
                     for (Queue<String> fileList : receiverFileList.values()) {
                         fileList.add(actualFileName);
                     }
                 }
+                // Size of the file
                 long size = dis.readLong();
+                // Buffer to store part of the file
                 byte[] b = new byte[1024 * 1024];
+                // Bytes read so far
                 long count = 0;
+                // Stream to write the file
                 FileOutputStream fos = new FileOutputStream(downloadsFolder + "/" + actualFileName);
                 while (true) {
+                    // Number of bytes read
                     int r = dis.read(b, 0, (size - count) > 1024 * 1024 ? 1024 * 1024 : (int) (size - count));
                     fos.write(b, 0, r);
                     count += r;
@@ -82,8 +94,10 @@ class MiniClient implements Runnable {
                 fos.close();
             }
             if (clientFileListMap == null) {
+                // Client
                 JOptionPane.showMessageDialog(null, "Received the files");
             } else {
+                // Server
                 clientFileListMap.putAll(receiverFileList);
             }
             dis.close();
