c7ce83c5597e9001c47bfdf6ea646512d4ba5b48
AtishayJain15395 <AtishayJain15395@gmail.com>
2016-11-28 20:00:05 +0530

    Minor

--- src/inspire/ClientController.java
+++ src/inspire/ClientController.java
@@ -74,7 +74,7 @@ public class ClientController {
                                     clientModel = new ClientModel(hostName);
                                     clientView.setClientModel(clientModel);
                                     break;
-                                } catch (IOException ignored) {
+                                } catch (IOException e) {
                                 }
                             }
                         }
